fa42f74b4fe848a4de49a8be9e338a12cae03cdd
rinshad4u <rinshad4u@gmail.com>
2021-04-07 01:52:08 +0530

    a

--- custom/demo/src/com/hybris/ymkt/segmentation/facades/DemoCampaignRestrictionPopulatorFacade.java
+++ custom/demo/src/com/hybris/ymkt/segmentation/facades/DemoCampaignRestrictionPopulatorFacade.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
  */
-package org.demo.facades;
+package com.hybris.ymkt.segmentation.facades;
 
 import de.hybris.platform.cmsfacades.data.OptionData;
 import de.hybris.platform.servicelayer.config.ConfigurationService;
